db50fe4128003b6e974b0ae17ea9573c4d25e463
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-09 03:09:33 +0000

    Entry file

--- simsh.c
+++ simsh.c
@@ -4,24 +4,40 @@
  * main - Entry point of our program
  * @ac: argument count
  * @av: argument vector
+ * @env: the environment variable
  *
  * Return: Always 0 on (Success)
  */
 
-int main(int ac, char **av)
+int main(int ac, char **av, char **env)
 {
 	char *input_line = NULL;
 	char **args;
+	int line_num = 1, statcode = 0;
+
+	signal(SIGINT,  SIG_IGN);
 
 	while (true)
 	{
 		print_prompt();
 
-		args = read_and_tokenizeInput(input_line, ac);
+		args = read_and_tokenizeInput(input_line);
+		if (args && args[0])
+		{
+			if (strcmp(args[0], "exit") == 0)
+			{
+				handle_exit();
+			}
 
-		execute_cmd(args, av);
+			statcode = 0;
+			execute_cmd(args, av, env, line_num, &statcode);
+		}
+		statcode = 127;
 
-		free_tokens(args, ac);
+		line_num++;
+		free_tokens(args);
 	}
+
+	return (statcode);
 
 }
