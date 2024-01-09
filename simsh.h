fc53671fe1c40ce30f7c6ba039be4da8374cdb24
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-09 05:29:13 +0000

    Header file updated

--- simsh.h
+++ simsh.h
@@ -21,6 +21,6 @@ void free_tokens(char **args);
 char *getAbsPath(char *filename);
 char *getFullPath(char *filename, char *pathName);
 int checkForwardSlash(char *filename);
-void handle_exit(void);
+void handle_exit(int exit_stat);
 
 #endif /* SIMSH_H */
