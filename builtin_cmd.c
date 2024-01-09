4cad21dab515a245042ebd824848f7f8ad9295ce
Ebenezer terabo <teraboebenezer@gmail.com>
2024-01-09 13:07:02 +0000

    built in file updated

--- builtin_cmd.c
+++ builtin_cmd.c
@@ -13,11 +13,3 @@ void handle_exit(int exit_stat)
 	exit(exit_stat);
 }
 
-/**
- * handle_builtin_cmd - A function that handles built in commands
- * @args: Arguments
- * @av: argument vector
- * @env: the environment variable
- *
- * Return: Always 0 on success, otherwise 1
- */
